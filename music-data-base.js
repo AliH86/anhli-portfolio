0230e5e8fb6798fb033e11d92ea5d4ba4f998de4
Ali Huynh <ali.readytostart@gmail.com>
2026-08-08 14:32:13 +0700

    Update track count and add new track to album

--- music-data-base.js
+++ music-data-base.js
@@ -57,14 +57,15 @@ window.PORTFOLIO_MUSIC = {
                 "sub": "Dân ca ba miền · Heritage Folk Series",
                 "cover": "uploads/toi-muon-om-tron-di-san.jpg",
                 "desc": "Đi từ làng nghề, di sản văn hoá đến những làn điệu dân gian ba miền, album kể lại chất liệu Việt bằng một hơi thở mới.",
-                "count": 6,
+                "count": 7,
                 "tracks": [
                   { "id": "69ede1f4-3725-467f-b424-beed636dabc1", "name": "IN MỘT NÉT TRANH", "dur": "4:54" },
                   { "id": "b6902a96-b14b-4317-80d7-fd9d78dcae05", "name": "VŨ ĐIỆU ĐẤT", "dur": "4:13" },
                   { "id": "de139955-724b-4ce5-a96a-7074eb5a4cde", "name": "CON CHI VỪA LÓ RA NÌ?", "dur": "4:14" },
                   { "id": "05a1cbff-5616-4190-bf73-cb62fd85a0d6", "name": "BUÔNG CÂU ĐỜN CA", "dur": "5:02" },
                   { "id": "99ecde35-d33e-4f83-ba1c-19cacad9ff8e", "name": "BÀI THƠ BỒNG BỀNH", "dur": "4:24" },
-                  { "id": "dd2d5a9c-424d-4605-9544-24818bdd5e35", "name": "Ơ LÚA RÃY ƠI", "dur": "3:43" }
+                  { "id": "dd2d5a9c-424d-4605-9544-24818bdd5e35", "name": "Ơ LÚA RÃY ƠI", "dur": "3:43" },
+                  { "id": "4fec75bb-1b1b-47e3-b67e-a910881afa66", "name": "ÂN TÌNH MẸ QUÊ", "dur": "4:35" }
                             ]
     },
     {
